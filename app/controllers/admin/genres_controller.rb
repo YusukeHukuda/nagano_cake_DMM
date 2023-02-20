a97093cd1e535407d387d5e6eedaec79382dad21
EC2 Default User <ec2-user@ip-172-31-10-205.ap-northeast-1.compute.internal>
2023-02-20 13:13:53 +0000

    [Update]nagano_cake

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,9 +1,9 @@
-class GenresController < ApplicationController
+class Admin::GenresController < ApplicationController
   def index
     @genres = Genre.all
     @genre = Genre.new
   end
-  
+
   def create
     genre = Genre.new(genre_params)
     genre.save
