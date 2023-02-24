fda045f9e90a01ab83dfb69752eaa70154734955
EC2 Default User <ec2-user@ip-172-31-10-205.ap-northeast-1.compute.internal>
2023-02-24 13:12:46 +0000

    [Update]nagano_cake

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -15,12 +15,25 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
+    @genres = Genre.all
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_items_path
+    else
+      render :edit
+    end
+  end
 
+ private
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active, :image)
   end
 end
