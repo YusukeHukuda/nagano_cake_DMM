3f59aa548bcc431f017357ef521e9c5a746a71d9
EC2 Default User <ec2-user@ip-172-31-10-205.ap-northeast-1.compute.internal>
2023-02-27 15:22:19 +0000

    [Update]nagano_cake

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_17_101239) do
+ActiveRecord::Schema.define(version: 2023_02_27_151721) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -78,6 +78,13 @@ ActiveRecord::Schema.define(version: 2023_02_17_101239) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "is_deleted", default: false
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
+    t.string "postal_code"
+    t.string "address"
+    t.string "telephone_number"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
